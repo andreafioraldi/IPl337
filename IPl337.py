edef552a38b23c4c820b274aab7aad1b28acaeec
andreafioraldi <andreafioraldi@gmail.com>
2017-06-25 20:15:59 +0200

    Lasagne

--- IPl337.py
+++ IPl337.py
@@ -7,23 +7,22 @@ import json
 app = flask.Flask(__name__)
 
 VIRUSTOTAL_API_KEY = '97f4945cb7c4838c3d8348615e81cc292de1b5cd2a7be4a3772d0475815ee9f6'
-virustotal_header = {
-    "Accept-Encoding": "gzip, deflate",
-    "User-Agent" : "gzip,  My Python requests library example client or username"
-}
 
 def virustotal_scan(url):
-    params = {'apikey': VIRUSTOTAL_API_KEY, 'resource':url, 'scan':'1'}
-    response = requests.post('https://www.virustotal.com/vtapi/v2/url/report', params = params, headers = virustotal_header)
-    json_response = response.json()
-    return json.dumps(json_response, indent=2)
+    params = {
+        'apikey': VIRUSTOTAL_API_KEY,
+        'resource': url,
+        'scan': '1'
+    }
+    response = requests.post('https://www.virustotal.com/vtapi/v2/url/report', params = params)
+    return response.json()
 
 @app.route('/')
 def index_page():
     input_url = flask.request.args.get("url", "")
     if input_url != "":
-        info = virustotal_scan(input_url)
-        return flask.render_template("url.html", input_url = input_url, info = info)
+        vtotal = virustotal_scan(input_url)
+        return flask.render_template("url.html", input_url = input_url, vtotal = vtotal, vtotal_s=json.dumps(vtotal, indent=2))
     else:
         return """
 <!DOCTYPE html>
@@ -41,10 +40,4 @@ def index_page():
 </body>
 </html>
 """
-'''@app.route('/url', methods=['GET', 'POST'])
-def url_page():
-    if request.method == 'POST':
-        return request.form['inputbox']
-    else:
-        return'''
 
